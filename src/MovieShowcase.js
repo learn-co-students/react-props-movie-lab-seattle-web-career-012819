6d9911f02eacfd460db61e60a498b51dc44520e5
allen-woods <allen.woods@flatironschool.com>
2019-04-05 09:22:14 -0700

    First commit, included unique keys.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -3,15 +3,18 @@ import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
-
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
+    let keyIdx = 1
+    return movieData.map(card => (
+        <MovieCard title={card.title} IMDBRating={card.IMDBRating} genres={card.genres} poster={card.poster} key={keyIdx++} />
+    ))
   }
 
   render() {
+    const cards = this.generateMovieCards()
     return (
       <div id="movie-showcase">
-        {this.generateMovieCards()}
+        {cards}
       </div>
     )
   }
